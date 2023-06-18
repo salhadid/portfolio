d2fb350b17677cffa967d16b02be61c00a186eb2
smalhadid <smalhadid@gmail.com>
2023-06-18 16:07:20 -0400

    setting up navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+// images
+import Logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+    return (
+        <header className="py-8">
+            <div className="container mx-auto">
+                <div>
+                    {/* logo */}
+                    <a href="#">
+                        <img src={Logo} alt="" />
+                    </a>
+                    <button>Work with me</button>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
