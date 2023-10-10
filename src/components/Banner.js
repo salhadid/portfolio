eef18d454b500f2641e8622c0db896497d06390a
smalhadid <smalhadid@gmail.com>
2023-10-10 09:50:11 -0400

    updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "../assets/avatar.svg";
+import Image from "../assets/photo.png";
 import { FaGithub, FaLinkedin, FaGitlab } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
